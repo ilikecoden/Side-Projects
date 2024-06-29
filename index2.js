34efcb921f46cc4c0802ae57a01048a37610131a
Christian <168371905+ilikecoden@users.noreply.github.com>
2024-06-29 17:50:22 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,23 @@
+/* Login Java Script*/
+
+var ichBinSoGroß = 16;
+
+if (ichBinSoGroß > 15 ){
+
+    document.write( "Ich bin größer als 15");
+
+} else if (ichBinSoGroß > 10){
+
+    document.write("Ich bin größer als 10");
+
+} else if (ichBinSoGroß > 5){
+
+    document.write(" Ich bin größer als 5 ") ;
+
+} else if (ichBinSoGroß < 30 ){
+
+    document.write(" Ich bin kleiner als 30 ")
+
+}
+
+
